d9aa158b022dc9d26895e62d2784345d9f2a0193
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2021-09-25 14:30:15 +0800

    asdf

--- codeitsuisse/routes/stonks.py
+++ codeitsuisse/routes/stonks.py
@@ -1,14 +1,14 @@
 import logging
 import json
 
-from flask import request, jsonify;
+from flask import request, jsonify
 
-from codeitsuisse import app;
+from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
 @app.route('/stonks', methods=['POST'])
-def evaluate():
+def stonks():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     energy = data.get("energy")
