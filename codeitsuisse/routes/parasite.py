0d66359f19e2dfec3b2d31d0eeeb73a04c60f91d
PANhuihuihuihui <phjhk@connect.hku.hk>
2021-09-25 18:37:17 +0800

    parasite1

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -42,6 +42,28 @@ class Board:
                     visited[go_x][go_y] = True
                     queue.append( (go_x,go_y) )
         for i,j in self.interest:
+            if self.board[i][j] != 0:
+                p1[str(i)+','+str(j)] = board[i][j]
+            else:
+                p1[str(i)+','+str(j)] = -1
+    def bfs2():
+        queue = []
+        px = [-1, 0, 1, 0]
+        py = [0, -1, 0, 1]
+        board = [ [ 100 for i in range(self.col)] for _ in range(self.row)]
+        visited = [ [ False for i in range(self.col)] for _ in range(self.row)]
+        board[self.start_x][self.start_y] = 0
+        visited[self.start_x][self.start_y] = True
+        queue.append( (self.start_x,self.start_y) )
+        while len(queue) > 0:
+            x,y = queue.pop()
+            for i in range(4):
+                go_x,go_y = x+px[i],y+py[i]
+                if go_x < self.row and go_x>0 and go_y < self.col and go_y > 0 and self.board[go_x][go_y] != 0 and not visited[go_x][go_y]  :
+                    board[go_x][go_y] = min( board[x][y]  + 1,board[x][y] )
+                    visited[go_x][go_y] = True
+                    queue.append( (go_x,go_y) )
+        for i,j in self.interest:
             if self.board[go_x][go_y] != 0:
                 p1[str(i)+','+str(j)] = board[i][j]
             else:
